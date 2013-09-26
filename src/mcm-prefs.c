e38c8017c3f4e5b24fef0e01af564f9d76814fcb
raveit65 <chat-to-me@raveit.de>
2013-09-26 15:24:18 +0200

    trivial: Port to GApplication API from git master

--- src/mcm-prefs.c
+++ src/mcm-prefs.c
@@ -130,7 +130,7 @@ static void
 mcm_prefs_close_cb (GtkWidget *widget, gpointer data)
 {
 	GApplication *application = (GApplication *) data;
-	g_application_quit (application, 0);
+	g_application_quit_with_data (application, NULL);
 }
 
 /**
@@ -3261,7 +3261,7 @@ main (int argc, char **argv)
 	g_option_context_free (context);
 
 	/* ensure single instance */
-	application = g_application_new_and_register ("org.gnome.ColorManager.Prefs", argc, argv);
+	application = g_application_new ("org.mate.ColorManager.Prefs", argc, argv);
 	g_signal_connect (application, "prepare-activation",
 			  G_CALLBACK (mcm_prefs_application_prepare_action_cb), NULL);
 
